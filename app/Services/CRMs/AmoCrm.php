665d003f7759f67d9d438d2afbf403185a4379e2
begimov <begimov@gmail.com>
2018-06-21 11:15:07 +0300

    AmoCrm's general functionality and handleLeadsAdd method has been implemented

--- app/Services/CRMs/AmoCrm.php
+++ app/Services/CRMs/AmoCrm.php
@@ -3,11 +3,30 @@
 namespace App\Services\CRMs;
 
 use App\Services\CRMs\Contracts\IAmoCrm;
+use Illuminate\Support\Facades\Log;
 
 class AmoCrm implements IAmoCrm
 {
     public function handleAction(array $payload)
     {
-        Log::debug('amoCRM:', $payload);
+        // Log::debug($payload['leads']['add'][0]['name']);
+
+        [$entity] = array_keys($payload);
+        [$action] = array_keys($payload[$entity]);
+
+        $method = 'handle' . ucfirst($entity) . ucfirst($action);
+        
+        if (method_exists($this, $method)) {
+            return $this->{$method}($payload, $entity, $action);
+        } else {
+            //
+        }
+    }
+
+    public function handleLeadsAdd($data, $entity, $action)
+    {
+        $leads = $data[$entity][$action];
+        
+        // queue
     }
 }
