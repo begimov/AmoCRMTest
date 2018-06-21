170cbe55f72265da9d450834a082d6cf1c58cc77
begimov <begimov@gmail.com>
2018-06-21 15:45:26 +0300

    basic vuex modules structure has been created

---
+++ resources/assets/js/store/index.js
@@ -0,0 +1,11 @@
+import leadslist from './modules/leadslist'
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules: {
+    leadslist
+  }
+})
