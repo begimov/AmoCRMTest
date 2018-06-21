75ef9f05de1b5475bdc6d31fe116519dd5f33e0b
begimov <begimov@gmail.com>
2018-06-21 09:11:07 +0300

    AmoCRMWebhookController has been implemented

--- app/Http/Controllers/Webhooks/AmoCRMWebhookController.php
+++ app/Http/Controllers/Webhooks/AmoCRMWebhookController.php
@@ -7,5 +7,16 @@ use App\Http\Controllers\Controller;
 
 class AmoCRMWebhookController extends Controller
 {
-    //
+    protected $amoCRM;
+
+    public function __construct(IAmoCrm $amoCRM)
+    {
+        $this->amoCRM = $amoCRM;
+    }
+
+    public function handleWebhook(Request $request)
+    {
+        $payload = $request->all();
+        $this->amoCRM->handleAction($payload);
+    }
 }
