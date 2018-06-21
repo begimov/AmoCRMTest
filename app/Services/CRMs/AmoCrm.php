ebe054ce82308afb5b8bb1092bde743b70731a11
begimov <begimov@gmail.com>
2018-06-21 09:40:25 +0300

    AmoCrm implementation has been created

---
+++ app/Services/CRMs/AmoCrm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services\CRMs;
+
+use App\Services\CRMs\Contracts\IAmoCrm;
+
+class AmoCrm implements IAmoCrm
+{
+    public function handle(array $payload)
+    {
+        # code...
+    }
+}
